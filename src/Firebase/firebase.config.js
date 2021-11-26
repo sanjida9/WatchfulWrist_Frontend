dd3866887045f0fe36314b04a0b952695ade86c0
sanjida9 <afrinsanjida03@gmail.com>
2021-11-26 17:56:17 +0600

    aaa

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD6pqvlB-3wQg7bO-PEVToRcF7hTrygQ2g",
-  authDomain: "watchful-wrist.firebaseapp.com",
-  projectId: "watchful-wrist",
-  storageBucket: "watchful-wrist.appspot.com",
-  messagingSenderId: "315289658244",
-  appId: "1:315289658244:web:382c7a9b8c0ba93426f3d1",
+  apiKey: "AIzaSyD0pTZpLBiPcDK26nhXEhfprbDwQjZJGwA",
+  authDomain: "watchfulwrist2.firebaseapp.com",
+  projectId: "watchfulwrist2",
+  storageBucket: "watchfulwrist2.appspot.com",
+  messagingSenderId: "780282652837",
+  appId: "1:780282652837:web:716a0fc124609484e6054c",
 };
 
 export default firebaseConfig;
